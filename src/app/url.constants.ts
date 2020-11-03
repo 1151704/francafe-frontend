1ea20f123f85f24d4aaa4cabc04044cb767eda25
Omar Ramón Montes <omarmontes.879@gmail.com>
2020-11-03 08:02:56 -0500

    Url backend dinamico

--- src/app/url.constants.ts
+++ src/app/url.constants.ts
@@ -1,9 +1,15 @@
+const relacionUrls = [
+   {client: 'http://localhost:4200', backend: 'http://localhost:8080/'},
+   {client: 'https://ayd-pqrs-ufps.herokuapp.com', backend: 'https://ayd-pqrs-ufps-back.herokuapp.com'},
+]
+
 const getURlBackend = () => {
-
-   let client = `${window.location.protocol}//${window.location.hostname}`
-   let portBackend = 8080;
-
-   return `${client}:${portBackend}/`
+   let client = window.location.origin
+   let actual = relacionUrls.filter(item => item.client == client)
+   if (actual && actual.length==1) {
+      return actual[0].backend
+   }
+   return client
 }
 
 export const REST = getURlBackend();
